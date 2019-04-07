7be0c361e1136fbc012dc329a590937a5bcd109c
Andreea Stirbu <stirbu.an@yahoo.com>
2019-04-07 17:23:48 +0100

    refactoring

--- client/app/main.js
+++ client/app/main.js
@@ -3,10 +3,8 @@ import searchBarComponent from '/app/components/searchBar.js'
 
 const mainContainer = new Container('#mainContainer');
 const wordsList = wordListComponent;
-
 mainContainer.addComponent(wordsList);
 mainContainer.showComponent(wordsList.name);
-
 
 const searchContainer = new Container('#searchContainer');
 const searchBar = searchBarComponent;
